701be5263ae1f9bbe1b2abc8f272218598b4b138
royalclassy <nugraheniesterlita@gmail.com>
2023-05-19 13:31:40 +0700

    first push

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  
+
+  return (
+    <div className="flex justify-center">
+      <input type="text" className="font-bold bg-[#F5F5F5] text-xl p-2 px-5 focus:outline-2 focus: outline-[#A445ED] rounded-xl w-[90%]" placeholder="test"/>
+      
+    </div>
+  )
+}
+
+export default App
